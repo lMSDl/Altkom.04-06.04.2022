dcd0cdd3a4a0511a9f88f148794bd42268d2fa3b
Paweł Łęczycki <pawel@msd.info.pl>
2022-04-04 12:08:50 +0200

    Liskov Substitution Principle

---
+++ SOLID/III_LSP_2.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SOLID.L
+{
+    public abstract class Shape
+    {
+        public abstract int Area { get; }
+    }
+
+    public class Square : Rectangle //To dziedziczenie łamie zasadę
+    {
+        public override int A { set => base.A = base.B = value; }
+        public override int B { set => base.B = base.A = value; }
+    }
+
+    public class Rectangle : Shape
+    {
+        public virtual int A { get; set; }
+        public virtual int B { get; set; }
+
+        public override int Area => A * B;
+    }
+}
--- SOLID/Program.cs
+++ SOLID/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SOLID.L;
+using System;
 
 namespace SOLID
 {
@@ -6,6 +7,14 @@ namespace SOLID
     {
         static void Main(string[] args)
         {
+            CalcRectangleArea(new Square(), 2, 5);
+        }
+
+        static void CalcRectangleArea(Rectangle rectangle, int sizeA, int sizeB)
+        {
+            rectangle.A = sizeA;
+            rectangle.B = sizeB;
+            Console.WriteLine($"{sizeA} * {sizeB} = {rectangle.Area}");
         }
     }
 }
