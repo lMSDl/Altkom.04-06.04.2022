dbdc8ff1acf577d3ea068d6f1db4c0aa0c2d0a3e
Paweł Łęczycki <pawel@msd.info.pl>
2022-04-05 14:33:31 +0200

    PrivateClassData

---
+++ DesignPatterns/Structural/PrivateClassData/SomeClass.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns.Structural.PrivateClassData
+{
+    public class SomeClass
+    {
+        private SomeClassData data;
+
+        public SomeClass(string @string, int @int)
+        {
+            //this.@string = @string;
+            //this.@int = @int;
+            data = new SomeClassData(@string, @int);
+        }
+
+        public void SomeMethid()
+        {
+            data.GetInt();
+            data.GetString();
+        }
+    }
+}
---
+++ DesignPatterns/Structural/PrivateClassData/SomeClassData.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns.Structural.PrivateClassData
+{
+    public class SomeClassData
+    {
+        private string @string;
+        private int @int;
+
+        public SomeClassData(string @string, int @int)
+        {
+            this.@string = @string;
+            this.@int = @int;
+        }
+
+        public string GetString()
+        {
+            return @string;
+        }
+        public int GetInt()
+        {
+            return @int;
+        }
+    }
+}
