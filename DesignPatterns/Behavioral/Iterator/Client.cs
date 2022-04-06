6d8b98102b28edaf837087cfe1e659dcc1edc318
Paweł Łęczycki <pawel@msd.info.pl>
2022-04-06 09:31:56 +0200

    Iterator

---
+++ DesignPatterns/Behavioral/Iterator/Client.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns.Behavioral.Iterator
+{
+    public class Client
+    {
+        public static void Execute()
+        {
+            var list = new List<string> { "a", "b", "c", "d", "e" };
+
+            var buffer = new Buffer<string>(list);
+            var bufferIterator = buffer.GetEnumerator();
+            while(bufferIterator.MoveNext())
+            {
+                var item = bufferIterator.Current;
+                Console.WriteLine($"{item[0]}{item[1]}");
+            }
+
+            foreach (var item in buffer)
+            {
+                Console.WriteLine($"{item[0]}{item[1]}");
+            }
+        }
+    }
+}
--- DesignPatterns/Program.cs
+++ DesignPatterns/Program.cs
@@ -6,7 +6,7 @@ namespace DesignPatterns
     {
         static void Main(string[] args)
         {
-            Behavioral.Command.Client.Execute();
+            Behavioral.Iterator.Client.Execute();
             Console.ReadLine();
         }
     }
